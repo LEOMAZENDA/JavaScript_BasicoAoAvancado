d490fa9f8034b2e5a549595d1fe72e83253e0280
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-09-01 08:52:12 +0100

    Variáveis

---
+++ Fundamentos do JS/aula6/index.js
@@ -0,0 +1,34 @@
+//Trabalhando com Variavéis no JS
+
+let nome = 'João Manuel';
+var nome2 = 'Maria'; // var é mais entigo
+
+console.log(nome,'nasceu em 1984.')
+console.log('Em 2000',nome, 'conheceu',nome2)
+console.log(nome, 'casou-se com',nome2, 'em 2012.')
+console.log(nome2, 'teve 1 filho com',nome)
+console.log('O filho de',nome,'se chama Eduaro')
+
+let nome3;
+
+nome3 = 'Sagrado'
+
+nome2 = 'Pinto'
+console.log(nome3)
+console.log(nome2)
+
+
+// Não se deve criar variáveis com as palavras reservadas da linguagem
+/* Exemplos a Baixo 
+let if;*/
+
+// Não se deve criar variáveis cuja o nome começ a com número
+/* Exemplo
+let 1nome;*/
+// Variváveis devem ter nomes expressivos ou significativos
+//Utilizamos camelCase para Nomes compostos
+/*case-sensitive (significa que levras maiusculas e minusculas fazem diferença nos nomes das variáveis)
+Exemplo: 
+nomeCliente é diferente de nomecliente*/
+
+nomeCliente2 = 'Pinto Luis'
