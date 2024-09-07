d4c63c37997b41a08d56b03d0f9ccc5383924f57
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-09-07 05:04:44 +0100

    Operadores e Conserções

---
+++ Fundamentos do JS/aula9/index.js
@@ -0,0 +1,32 @@
+/**
+ * Artitimétricos (+; - ;* e /)
+ * Adição ou contatenação
+ * Potenciação **
+ * Resto da divisão %
+ * Incremento ++
+ * Decremento --
+ * Ordem da precedência 
+ * ()
+ * **
+ *  / ou *
+ *  + ou -
+ */
+
+const num1 = 5;
+const num2 = 10; 
+
+// console.log(num1 ** num2) // ** é potenciação 
+// Resto da vivisão 
+
+const div1 = 10;
+const div2 = 3;
+console.log(div1 % div2, num1 + num2) 
+
+//CONSERÇÕES para NaN
+parseInt();
+parseFloat();
+// const numSttring = Number ("5");
+
+const numSttring = parseInt("5");
+const numInt = 7; 
+console.log(numInt + numSttring);
