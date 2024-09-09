6850d4ae26553d1c1c57082248712f9e3c566603
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-09-09 05:57:47 +0100

    37 Operadores de comparação

---
+++ 02. Lógica de programação com JS/aula21/index.js
@@ -0,0 +1,25 @@
+/*
+OPERADORES DE COMPARAÇÃO 
+> maior que
+< monor que 
+>= maior ou igual que 
+<= menor ou igual que 
+==  igualdade (valor) **************** não usar 
+=== igualdade escrita (valor e tipo)
+!=  diferente (valor) *************** não usar 
+!== igualdade escrita (valor e tipo)
+*/
+
+// console.log(10 > 5);
+const num1 = 10;
+const num2 = '10';
+const comp1 = num1 == num2; 
+const comp2 = num1 === num2; 
+console.log(comp1, comp2);
+
+
+const num3 = 10;
+const num4 = '10';
+const comp3 = num1 != num2; 
+const comp4 = num1 !== num2; 
+console.log(comp2, comp3);
