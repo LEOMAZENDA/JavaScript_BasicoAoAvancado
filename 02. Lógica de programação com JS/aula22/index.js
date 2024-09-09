0eeb5d65612d19c34445a88b211e921d66491113
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-09-09 06:31:53 +0100

    38 Operadores Lógicos

---
+++ 02. Lógica de programação com JS/aula22/index.js
@@ -0,0 +1,27 @@
+/*
+OPERADORES LÓGICOS
+&& -> AND -> E - Todas as expressões precisam ser verdadeiras 
+|| -> OR -> OU - So precisa dar certo uma das expressões
+! -> NOT -> NÃO
+ */
+
+// //AND
+// const expVerdadeAnd = true && true && true && true;
+// const expFalsaAndFalsa = true && true && true && false;
+// console.log(expVerdadeAnd, expFalsaAndFalsa);
+
+// //OR
+// const expVerdadeOr = true || true || true || true;
+// const expVerdadeOrFalsa = false || false || false || false;
+// console.log(expVerdadeOr, expVerdadeOrFalsa);
+
+// const usuario = 'Leonildo' // vindo do form
+// const senha = '123456' // vindo do form
+
+// const logar = usuario === 'Leonildo' && senha === '123456';
+// console.log(logar);
+
+//NOT
+console.log(true)
+console.log(!true);
+console.log(!!true);
