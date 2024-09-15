284446d1df9385acd84d01f26fb9c902b7a14def
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-09-15 08:46:44 +0100

    Ternario fim

---
+++ 02. Lógica de programação com JS/aula27/index.js
@@ -0,0 +1,24 @@
+// Operador ternário "?" 
+
+const pontuacaoUsuario = 900; 
+
+// Condição normal
+if (pontuacaoUsuario >= 1000){
+    console.log('Usuário VIP');
+} else {
+    console.log('Usuário normal');
+}
+//-------------------------------------------------------------
+
+// usando o Operador ternário "?" 
+// condição) ? 'Valor para verdadeiro' : 'Valor para falso'; 
+const nivielUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+
+const corUsuario = null; //pode mudar o valor aqui para uma cor 
+const corPadrao = corUsuario || 'Preta';
+
+console.log('Nivel e cor do Usuário ==> ',nivielUsuario, corPadrao);
+
+
+//-------------------------------------------------------------
+
