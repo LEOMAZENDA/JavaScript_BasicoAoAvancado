441ca173e88907f497edb1ad04f86a163154ddc8
LEOMAZENDA <eng.leomazenda@gmail.com>
2024-09-15 08:47:28 +0100

    fi

--- 02. Lógica de programação com JS/aula27/index.js
+++ 02. Lógica de programação com JS/aula27/index.js
@@ -1,17 +1,19 @@
 // Operador ternário "?" 
 
-const pontuacaoUsuario = 900; 
+// const pontuacaoUsuario = 900; 
 
-// Condição normal
-if (pontuacaoUsuario >= 1000){
-    console.log('Usuário VIP');
-} else {
-    console.log('Usuário normal');
-}
+// // Condição normal
+// if (pontuacaoUsuario >= 1000){
+//     console.log('Usuário VIP');
+// } else {
+//     console.log('Usuário normal');
+// }
 //-------------------------------------------------------------
 
 // usando o Operador ternário "?" 
 // condição) ? 'Valor para verdadeiro' : 'Valor para falso'; 
+const pontuacaoUsuario = 900; 
+
 const nivielUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
 
 const corUsuario = null; //pode mudar o valor aqui para uma cor 
