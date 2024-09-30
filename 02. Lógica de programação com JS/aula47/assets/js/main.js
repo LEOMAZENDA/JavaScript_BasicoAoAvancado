2cafb71618b10a1225851c2aca04e05833658ebf
Leonildo Vivaldo Mazenda <88457638+LEOMAZENDA@users.noreply.github.com>
2024-09-30 16:05:59 +0100

    fechando

--- 02. Lógica de programação com JS/aula47/assets/js/main.js
+++ 02. Lógica de programação com JS/aula47/assets/js/main.js
@@ -1,16 +1,42 @@
+
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+let segundos = 0; 
+let timmer; 
+
+
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-PT', {
+        hour12: false, 
+        timeZone: 'UTC'
+    });
+}
+
+
+function iniciaRelogio(){
+    timmer = setInterval(function(){
+        segundos ++; 
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+}
 
 iniciar.addEventListener('click', function(e){
-    relogio.innerHTML = 'Cliquei no iniciar';
+    relogio.classList.remove('pausado'); //Isso está a remover a classe pausado do HTML
+    clearInterval(timmer);
+    iniciaRelogio();
 });
 
 pausar.addEventListener('click', function(e){
-    // alert('Oi.. Clicado no pausar');
+    clearInterval(timmer);
+    relogio.classList.add('pausado'); //Isso está a adiciona a classe pausado do HTML
 });
 
 zerar.addEventListener('click', function(e){
-    alert('Oi.. Clicado no zerar');
+    relogio.classList.remove('pausado'); //Isso está a remover a classe pausado do HTML
+    clearInterval(timmer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
 });
