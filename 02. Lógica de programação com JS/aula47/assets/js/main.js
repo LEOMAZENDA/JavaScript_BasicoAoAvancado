e90f84caf329dddcebab0ca382f13169a875b55c
Leonildo Vivaldo Mazenda <88457638+LEOMAZENDA@users.noreply.github.com>
2024-10-08 11:17:14 +0100

    66 Exercício - Criando um timer com setInterval

--- 02. Lógica de programação com JS/aula47/assets/js/main.js
+++ 02. Lógica de programação com JS/aula47/assets/js/main.js
@@ -1,42 +1,81 @@
+function relogio () {
+    let segundos = 0;
+    let timmer;
+    
+    function criaHoraDosSegundos(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-PT', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    }
+    
+    
+    function iniciaRelogio() {
+        timmer = setInterval(function () {
+            segundos++;
+            relogio.innerHTML = criaHoraDosSegundos(segundos);
+        }, 1000);
+    }
+    
+    /**
+     */
+    
+    //Segunda Solução 
+    const relogio = document.querySelector('.relogio');
+    
+    document.addEventListener('click', function (e) {
+        const elClicado = e.target;
+    
+        if (elClicado.classList.contains('zerar')) {
+            clearInterval(timmer);
+            relogio.innerHTML = '00:00:00';
+            relogio.classList.remove('pausado'); //Isso está a remover a classe pausado do HTML       
+            segundos = 0;
+        }
+        
+        if (elClicado.classList.contains('iniciar')) {
+            relogio.classList.remove('pausado'); //Isso está a remover a classe pausado do HTML
+            clearInterval(timmer);
+            iniciaRelogio();
+        }
+    
+        if (elClicado.classList.contains('pausar')) {
+            relogio.classList.add('pausado'); //Isso está a remover a classe pausado do HTML
+            clearInterval(timmer);   
+        }
+        
+    });
+}
+
+relogio();
+
+//------------------------------------------------------------------------------------------------------
+
+
+/*
+//Primeira solução 
 
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
-let segundos = 0; 
-let timmer; 
 
-
-function criaHoraDosSegundos(segundos) {
-    const data = new Date(segundos * 1000);
-    return data.toLocaleTimeString('pt-PT', {
-        hour12: false, 
-        timeZone: 'UTC'
-    });
-}
-
-
-function iniciaRelogio(){
-    timmer = setInterval(function(){
-        segundos ++; 
-        relogio.innerHTML = criaHoraDosSegundos(segundos);
-    }, 1000);
-}
-
-iniciar.addEventListener('click', function(e){
+iniciar.addEventListener('click', function (e) {
     relogio.classList.remove('pausado'); //Isso está a remover a classe pausado do HTML
     clearInterval(timmer);
     iniciaRelogio();
 });
 
-pausar.addEventListener('click', function(e){
+pausar.addEventListener('click', function (e) {
     clearInterval(timmer);
-    relogio.classList.add('pausado'); //Isso está a adiciona a classe pausado do HTML
+    relogio.classList.add('pausado');         //Isso está a adiciona a classe pausado do HTML
 });
 
-zerar.addEventListener('click', function(e){
-    relogio.classList.remove('pausado'); //Isso está a remover a classe pausado do HTML
+zerar.addEventListener('click', function (e) {
+    relogio.classList.remove('pausado');     //Isso está a remover a classe pausado do HTML
     clearInterval(timmer);
     relogio.innerHTML = '00:00:00';
     segundos = 0;
 });
+*/
